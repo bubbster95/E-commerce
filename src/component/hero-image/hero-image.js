4c43f3e117cfba51c2c081a2d437ae17ca652b9a
bubbster95 <stiles.billy@gmail.com>
2021-01-31 15:18:31 -0500

    test 7?

--- src/component/hero-image/hero-image.js
+++ src/component/hero-image/hero-image.js
@@ -13,26 +13,18 @@ class HeroImage extends React.Component {
     }
     componentDidMount() {
         let image = ['/assets/kenneth-hargrave-GjsWjLCIh5I-unsplash.jpeg', '/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg', '/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg', '/assets/thomas-galler-IvtbiWEVa-4-unsplash.jpeg', '/assets/ugur-peker-9Wuxjit62QU-unsplash.jpeg', '/assets/ugur-peker-9Wuxjit62QU-unsplash.jpeg']
-        let roll = this.diceRoll(image);
-        // let imageDiv = document.getElementById(`hero-image`)
+
         this.setState({
-            imageUrl: image[roll]
+            imageUrl: image[Math.floor((Math.random() * image.length))]
         })
-        // imageDiv.style.backgroundImage = `url('/assets/kenneth-hargrave-GjsWjLCIh5I-unsplash.jpeg')`
-        // imageDiv.style.backgroundImage = `url('${image[roll]}')`
     }
-
-    diceRoll(image) {
-        return Math.floor((Math.random() * image.length))
-    }
-
 
     render() {
         if (this.props.page) {
             return(
                 <div className='hero-container'>
                     <div className='hero-page-title'>{this.props.page}</div>
-                    <div className='hero-image' id='hero-image' style={{backgroundImage: `url("${this.state.imageUrl}")`}} alt='Beautiful vista of the wilderness.'></div>
+                    <div className='hero-image' id='hero-image' style={{backgroundImage: 'url("/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg")'}} alt='Beautiful vista of the wilderness.'></div>
                 </div>
             )
         } else {
