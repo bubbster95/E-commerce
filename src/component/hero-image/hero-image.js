30467a96a2ec76c1e3efd4b7497f4df11e84f2ec
bubbster95 <stiles.billy@gmail.com>
2021-01-31 14:26:32 -0500

    Test 3

--- src/component/hero-image/hero-image.js
+++ src/component/hero-image/hero-image.js
@@ -5,12 +5,12 @@ import './hero-image.css'
 
 class HeroImage extends React.Component {
     componentDidMount() {
-        // let image = ['/assets/kenneth-hargrave-GjsWjLCIh5I-unsplash.jpeg', '/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg', '/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg', '/assets/thomas-galler-IvtbiWEVa-4-unsplash.jpeg', '/assets/ugur-peker-9Wuxjit62QU-unsplash.jpeg', '/assets/ugur-peker-9Wuxjit62QU-unsplash.jpeg']
-        // let roll = this.diceRoll(image);
+        let image = ['/assets/kenneth-hargrave-GjsWjLCIh5I-unsplash.jpeg', '/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg', '/assets/intricate-explorer-idfytKeoD0s-unsplash.jpeg', '/assets/thomas-galler-IvtbiWEVa-4-unsplash.jpeg', '/assets/ugur-peker-9Wuxjit62QU-unsplash.jpeg', '/assets/ugur-peker-9Wuxjit62QU-unsplash.jpeg']
+        let roll = this.diceRoll(image);
         let imageDiv = document.getElementById(`hero-image`)
 
-        imageDiv.style.backgroundImage = `/assets/kenneth-hargrave-GjsWjLCIh5I-unsplash.jpeg`
-        // imageDiv.style.backgroundImage = `url(${image[roll]})`
+        // imageDiv.style.backgroundImage = `url('/assets/kenneth-hargrave-GjsWjLCIh5I-unsplash.jpeg')`
+        imageDiv.style.backgroundImage = `url('${image[roll]}')`
     }
 
     diceRoll(image) {
